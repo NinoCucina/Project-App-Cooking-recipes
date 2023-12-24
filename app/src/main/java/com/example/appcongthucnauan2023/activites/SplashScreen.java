d8ef9d720f719e03e3c6bb4ff176c124e0393ffd
NinoCucina <lpham6038@gmail.com>
2023-12-24 21:10:43 +0700

    project

---
+++ app/src/main/java/com/example/appcongthucnauan2023/activites/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.appcongthucnauan2023.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.appcongthucnauan2023.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreen.this, Dashboard.class));
+            }
+        },3000);
+
+
+
+    }
+}
